63f3f24507f51f8e149674d1dc980838ee962694
João Lucas <joaolucas_oh@outlook.com>
2026-06-18 14:19:56 -0300

    Initial Commit

---
+++ InsulisisCheck/InsulisisCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsulisisCheckApp.swift
+//  InsulisisCheck
+//
+//  Created by joaolucas on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct InsulisisCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
